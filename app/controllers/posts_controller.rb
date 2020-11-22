f572b249345887f32b65f6f6f19707c6b584cc75
Fanny Rojon <fanny.rojon22@gmail.com>
2020-11-22 17:08:12 +0100

    update action posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,11 +19,19 @@ class PostsController < ApplicationController
     end
   end
 
-
-
-
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post updated"
+      redirect_to post_path(@post)
+    else
+      flash[:notice] = "Error updating post"
+      render :edit
+    end
   end
 
   private
