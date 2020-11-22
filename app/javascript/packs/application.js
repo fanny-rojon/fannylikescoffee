e4030154c7b93c5b79bf834404c2c29d647b15b1
Fanny Rojon <fanny.rojon22@gmail.com>
2020-11-22 16:55:44 +0100

    gems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,8 @@ require("channels")
 
 // External imports
 import "bootstrap";
+//= require jquery
+//= require bootstrap-sprockets
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
