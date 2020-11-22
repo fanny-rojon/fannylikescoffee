13a9b33ab11fe169ddac08d1d718e0a7c3e1e1ea
Fanny Rojon <fanny.rojon22@gmail.com>
2020-11-22 20:52:09 +0100

    Category controller show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,9 @@
 class CategoriesController < ApplicationController
   def index
   end
+
+  def show
+    @category = Category.find(params["id"].to_i)
+    @posts = @category.posts.reverse
+  end
 end
